5ccf1e7acf9d331163d238818ebc5767242b4b07
Yoann <80095031+YoannPierkot@users.noreply.github.com>
2021-06-11 13:20:01 +0200

    vrezvega

--- MythsAndSteel/Assets/MetaGame/Scripts/Player/Player.cs
+++ MythsAndSteel/Assets/MetaGame/Scripts/Player/Player.cs
@@ -83,10 +83,6 @@ public class Player
     /// </summary>
     /// <returns></returns>
     public bool OrgoneExplose(){
-      
-        
-         
-      
         return OrgoneValue > 5 ? true : false;
     }
     
@@ -110,22 +106,8 @@ public class Player
       
         if (OrgoneExplose() && !GameManager.Instance.ChooseUnitForEvent)
         {
-            
-            GameManager.Instance.DoingEpxlosionOrgone = true;
-            List<GameObject> unitList = player == 1 ? PlayerScript.Instance.UnitRef.UnitListRedPlayer : PlayerScript.Instance.UnitRef.UnitListBluePlayer;
-
-
-            GameManager.Instance.StartEventModeUnit(4, player == 1 ? true : false, unitList, "Explosion d'orgone", "Êtes-vous sur de vouloir infliger des dégâts à ces unités?", true);
-            GameManager.Instance._eventCall += GiveDamageToUnitForOrgone;
-       
-            if (player == 1) GameManager.Instance._eventCallCancel += CancelOrgoneP1;
-            else GameManager.Instance._eventCallCancel += CancelOrgoneP2;
-            PLayerOrgoneExplose = player;
-
-            if (OrgoneExploseCancel == true)
-            {
+            if (OrgoneExploseCancel == true){
                 UpdateOrgoneUI(player);
-            
                 OrgoneExploseCancel = false;
             }
           
@@ -135,8 +117,7 @@ public class Player
         {
             if (OrgoneExploseCancel == true)
             {
-
-            UpdateOrgoneUI(player);
+                UpdateOrgoneUI(player);
               
             }
           
@@ -147,24 +128,6 @@ public class Player
                 GameManager.Instance._waitToCheckOrgone();
             }
         }
-    }
-
-    /// <summary>
-    /// When Orgone explode
-    /// </summary>
-    void GiveDamageToUnitForOrgone(){
-        UIInstance.Instance.ActivateNextPhaseButton();
-
-        i = 0;
-
-        GameManager.Instance._waitEvent += DealOrgoneDamageToUnit;
-        GameManager.Instance.WaitToMove(0);
-
-        _OrgoneValue = 0;
-        _LastKnownOrgoneValue = 0;
-
-
-
     }
 
 
@@ -189,56 +152,8 @@ public class Player
             if(GameManager.Instance._waitToCheckOrgone != null)
             {
                 GameManager.Instance._waitToCheckOrgone();
-             
             }
         }
-    }
-
-    public void DealOrgoneDamageToUnit()
-    {
-        if (GameManager.Instance.UnitChooseList.Count > i)
-        {
-            GameManager.Instance.UnitChooseList[i].GetComponent<UnitScript>().TakeDamage(1, true);
-            i++;
-            GameManager.Instance._waitEvent -= DealOrgoneDamageToUnit;
-            GameManager.Instance._waitEvent += DealOrgoneDamageToUnit;
-            GameManager.Instance.WaitToMove(.035f);
-            
-        }
-   
-        else
-        {
-            GameManager.Instance._waitEvent -= DealOrgoneDamageToUnit;
-
-            GameManager.Instance.UnitChooseList.Clear();
-            OrgoneExploseCancel = true;
-            GameManager.Instance.IsCheckingOrgone = false;
-
-          
-
-            if (GameManager.Instance._waitToCheckOrgone != null)
-            {
-                GameManager.Instance._waitToCheckOrgone();
-             
-            }
-        }
-    }
-
-    /// <summary>
-    /// Si le joueur appuie sur le bouton annuler 
-    /// </summary>
-    void CancelOrgoneP1(){
-        CheckOrgone(1);
-       
-    }
-
-    /// <summary>
-    /// Si le joueur appuie sur le bouton annuler 
-    /// </summary>
-    void CancelOrgoneP2()
-    {
-        CheckOrgone(2);
-     
     }
 
     /// <summary>
